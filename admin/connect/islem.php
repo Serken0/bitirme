d9f2990e401903e1df4c96f7ad93981d5fb64a46
Serken Ali KARAKUŞ <spuduq@gmail.com>
2022-01-02 23:08:28 +0300

    first commit

---
+++ admin/connect/islem.php
@@ -0,0 +1,242 @@
+<?php
+ob_start();
+session_start();
+
+include 'baglan.php';
+include '../components/fonksiyon.php';
+
+if (isset($_POST['admingiris'])) {
+
+	$kullanici_mail=$_POST['kullanici_mail'];
+	$kullanici_password=md5($_POST['kullanici_password']);
+
+	$kullanicisor=$db->prepare("SELECT * FROM kullanici where kullanici_mail=:mail and kullanici_password=:password and kullanici_yetki=:yetki");
+	$kullanicisor->execute(array(
+		'mail' => $kullanici_mail,
+		'password' => $kullanici_password,
+		'yetki' => 5
+	));
+
+	echo $say=$kullanicisor->rowCount();
+
+	if ($say==1) {
+
+		$_SESSION['kullanici_mail']=$kullanici_mail;
+		header("Location:../components/index.php");
+		exit;
+
+	} else {
+
+		header("Location:../components/login.php?durum=no");
+		exit;
+	}
+}
+
+if (isset($_POST['kullaniciduzenle'])) {
+
+	$kullanici_id=$_POST['kullanici_id'];
+
+	$ayarkaydet=$db->prepare("UPDATE kullanici SET
+		kullanici_tc=:kullanici_tc,
+		kullanici_adsoyad=:kullanici_adsoyad,
+		kullanici_durum=:kullanici_durum
+		WHERE kullanici_id={$_POST['kullanici_id']}");
+
+	$update=$ayarkaydet->execute(array(
+		'kullanici_tc' => $_POST['kullanici_tc'],
+		'kullanici_adsoyad' => $_POST['kullanici_adsoyad'],
+		'kullanici_durum' => $_POST['kullanici_durum']
+	));
+
+	if ($update) {
+
+		Header("Location:../components/kullanici-duzenle.php?kullanici_id=$kullanici_id&durum=ok");
+
+	} else {
+
+		Header("Location:../components/kullanici-duzenle.php?kullanici_id=$kullanici_id&durum=no");
+	}
+}
+
+if (isset($_POST['kullanicibilgiguncelle'])) {
+
+	$kullanici_id=$_POST['kullanici_id'];
+
+	$ayarkaydet=$db->prepare("UPDATE kullanici SET
+		kullanici_adsoyad=:kullanici_adsoyad
+		WHERE kullanici_id={$_POST['kullanici_id']}");
+
+	$update=$ayarkaydet->execute(array(
+		'kullanici_adsoyad' => $_POST['kullanici_adsoyad']
+	));
+
+	if ($update) {
+
+		Header("Location:../../hesabim?durum=ok");
+
+	} else {
+
+		Header("Location:../../hesabim?durum=no");
+	}
+}
+
+if ($_GET['kullanicisil']=="ok") {
+
+	$sil=$db->prepare("DELETE from kullanici where kullanici_id=:id");
+	$kontrol=$sil->execute(array(
+		'id' => $_GET['kullanici_id']
+	));
+
+	if ($kontrol) {
+
+		header("location:../components/kullanici.php?sil=ok");
+
+	} else {
+
+		header("location:../components/kullanici.php?sil=no");
+
+	}
+}
+
+if (isset($_POST['kategoriduzenle'])) {
+
+	$kategori_id=$_POST['kategori_id'];
+	$kategori_seourl=seo($_POST['kategori_ad']);
+
+	
+	$kaydet=$db->prepare("UPDATE kategori SET
+		kategori_ad=:ad,
+		kategori_sira=:sira
+		WHERE kategori_id={$_POST['kategori_id']}");
+	$update=$kaydet->execute(array(
+		'ad' => $_POST['kategori_ad'],
+		'sira' => $_POST['kategori_sira']		
+	));
+
+	if ($update) {
+
+		Header("Location:../components/kategori-duzenle.php?durum=ok&kategori_id=$kategori_id");
+
+	} else {
+
+		Header("Location:../components/kategori-duzenle.php?durum=no&kategori_id=$kategori_id");
+	}
+}
+
+if (isset($_POST['kategoriekle'])) {
+
+	$kategori_seourl=seo($_POST['kategori_ad']);
+
+	$kaydet=$db->prepare("INSERT INTO kategori SET
+		kategori_ad=:ad,
+		kategori_sira=:sira
+		");
+	$insert=$kaydet->execute(array(
+		'ad' => $_POST['kategori_ad'],
+		'sira' => $_POST['kategori_sira']		
+	));
+
+	if ($insert) {
+
+		Header("Location:../components/kategori.php?durum=ok");
+
+	} else {
+
+		Header("Location:../components/kategori.php?durum=no");
+	}
+}
+
+if ($_GET['kategorisil']=="ok") {
+	
+	$sil=$db->prepare("DELETE from kategori where kategori_id=:kategori_id");
+	$kontrol=$sil->execute(array(
+		'kategori_id' => $_GET['kategori_id']
+	));
+
+	if ($kontrol) {
+
+		Header("Location:../components/kategori.php?durum=ok");
+
+	} else {
+
+		Header("Location:../components/kategori.php?durum=no");
+	}
+}
+
+if ($_GET['urunsil']=="ok") {
+	
+	$sil=$db->prepare("DELETE from urun where urun_id=:urun_id");
+	$kontrol=$sil->execute(array(
+		'urun_id' => $_GET['urun_id']
+	));
+
+	if ($kontrol) {
+
+		Header("Location:../components/urun.php?durum=ok");
+
+	} else {
+
+		Header("Location:../components/urun.php?durum=no");
+	}
+}
+
+if (isset($_POST['urunekle'])) {
+
+	$urun_seourl=seo($_POST['urun_ad']);
+
+	$kaydet=$db->prepare("INSERT INTO urun SET
+		kategori_id=:kategori_id,
+		urun_ad=:urun_ad,
+		urun_fiyat=:urun_fiyat,
+		urun_durum=:urun_durum,
+		urun_stok=:urun_stok	
+		");
+	$insert=$kaydet->execute(array(
+		'kategori_id' => $_POST['kategori_id'],
+		'urun_ad' => $_POST['urun_ad'],		
+		'urun_fiyat' => $_POST['urun_fiyat'],				
+		'urun_durum' => $_POST['urun_durum'],
+		'urun_stok' => $_POST['urun_stok']
+
+	));
+
+	if ($insert) {
+
+		Header("Location:../components/urun.php?durum=ok");
+
+	} else {
+
+		Header("Location:../components/urun.php?durum=no");
+	}
+}
+
+if (isset($_POST['urunduzenle'])) {
+
+	$urun_id=$_POST['urun_id'];
+	$urun_seourl=seo($_POST['urun_ad']);
+
+	$kaydet=$db->prepare("UPDATE urun SET
+		kategori_id=:kategori_id,
+		urun_ad=:urun_ad,
+		urun_fiyat=:urun_fiyat,
+		urun_durum=:urun_durum,
+		urun_stok=:urun_stok	
+		WHERE urun_id={$_POST['urun_id']}");
+	$update=$kaydet->execute(array(
+		'kategori_id' => $_POST['kategori_id'],
+		'urun_ad' => $_POST['urun_ad'],
+		'urun_fiyat' => $_POST['urun_fiyat'],
+		'urun_durum' => $_POST['urun_durum'],
+		'urun_stok' => $_POST['urun_stok'],
+	));
+
+	if ($update) {
+
+		Header("Location:../components/urun-duzenle.php?durum=ok&urun_id=$urun_id");
+
+	} else {
+
+		Header("Location:../components/urun-duzenle.php?durum=no&urun_id=$urun_id");
+	}
+}
+?>
---
+++ bitirme.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 29 Ara 2021, 23:41:21
+-- Sunucu sürümü: 10.4.21-MariaDB
+-- PHP Sürümü: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `bitirme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori_ad` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kategori_ust` int(2) NOT NULL,
+  `kategori_sira` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `kategori_ad`, `kategori_ust`, `kategori_sira`) VALUES
+(11, 'Dizüstü Bilgisayar', 0, 1),
+(12, 'Tablet', 0, 2),
+(13, 'Cep Telefonu', 0, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanici`
+--
+
+CREATE TABLE `kullanici` (
+  `kullanici_id` int(11) NOT NULL,
+  `kullanici_zaman` datetime NOT NULL DEFAULT current_timestamp(),
+  `kullanici_tc` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_ad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_mail` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_gsm` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_password` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_adsoyad` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_yetki` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `kullanici_durum` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `kullanici`
+--
+
+INSERT INTO `kullanici` (`kullanici_id`, `kullanici_zaman`, `kullanici_tc`, `kullanici_ad`, `kullanici_mail`, `kullanici_gsm`, `kullanici_password`, `kullanici_adsoyad`, `kullanici_yetki`, `kullanici_durum`) VALUES
+(158, '2021-11-07 00:55:18', '12345678910', 'Serken', 'serken@gmail.com', '05555555555', 'e10adc3949ba59abbe56e057f20f883e', 'Serken Ali KARAKUŞ', '5', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `urun`
+--
+
+CREATE TABLE `urun` (
+  `urun_id` int(11) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `urun_ad` varchar(250) COLLATE utf8_turkish_ci NOT NULL,
+  `urun_fiyat` float(9,2) NOT NULL,
+  `urun_stok` int(11) NOT NULL,
+  `urun_durum` enum('0','1') COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `urun`
+--
+
+INSERT INTO `urun` (`urun_id`, `kategori_id`, `urun_ad`, `urun_fiyat`, `urun_stok`, `urun_durum`) VALUES
+(17, 12, 'Apple iPad Wi-Fi 32GB 9.7', 0.01, 100, '1'),
+(18, 12, 'Codegen CKL-097KL 9,7', 50.00, 100, '1'),
+(19, 13, 'Samsung Galaxy Note 4', 99.00, 50, '1');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Tablo için indeksler `kullanici`
+--
+ALTER TABLE `kullanici`
+  ADD PRIMARY KEY (`kullanici_id`);
+
+--
+-- Tablo için indeksler `urun`
+--
+ALTER TABLE `urun`
+  ADD PRIMARY KEY (`urun_id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kullanici`
+--
+ALTER TABLE `kullanici`
+  MODIFY `kullanici_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `urun`
+--
+ALTER TABLE `urun`
+  MODIFY `urun_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
